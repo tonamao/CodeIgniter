f6f940a69feb822abca0450ce9fef03370f9450c
shimba_m <mao.shimba.tona@gmail.com>
2019-09-29 23:22:39 +0900

    DB connection & insert

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = array(
+	'default',
+	'daifugo'
+);
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -79,6 +82,27 @@ $db['default'] = array(
 	'username' => 'user',
 	'password' => 'pass',
 	'database' => 'trump',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+$db['daifugo'] = array(
+	'dsn'	=> '',
+	'hostname' => 'mysql',
+	'username' => 'user',
+	'password' => 'pass',
+	'database' => 'daifugo',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
