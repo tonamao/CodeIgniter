b7c747d8bf79326e27d3d85eb252d5d43e4b244e
shimba_m <mao.shimba.tona@gmail.com>
2019-11-28 23:43:28 +0900

    display putted cards

--- application/views/daifugo/daifugo.php
+++ application/views/daifugo/daifugo.php
@@ -31,41 +31,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 	<div class="game-area">
 		<div class="cards" id="game-cards">
-	      	<?php for($i = 0; $i < 4; $i++) {?>
+			<?php $cnt = 0;?>
+	      	<?php foreach ($game_area_cards as $key => $cardIds) :?>
 	      		<?php 
-	      			$index = $i + 1;
+	      			$index = $cnt + 1;
 	      			$l = 0;
 	      			$t = 0;
-	      			if ($index % 2 == 0) {
-	      				$l = "50%";
-	      				$t = "26%";
-	      				if ($index % 4 == 0) {
-	      					$l = "42%";
-	      					$t = "30%";
+	      			if ($index % 2 == 0) {//2
+	      				$l = 50;
+	      				$t = 24;
+	      				if ($index % 4 == 0) {//4
+	      					$l = 43;
+	      					$t = 30;
 	      				}
-	      			} else {
-	      				$l = "44%";
+	      			} else {//1
+	      				$l = 45;
 	      				$t = "auto";
-	      				if ($index % 3 == 0) {
-	      					$l = "48%";
-	      					$t = "36%";
+	      				if (($index + 1) % 4 == 0) {//3
+	      					$l = 48;
+	      					$t = 36;
 	      				}
 	      			}
 	      		?>
-				<div class="card img" id="location-<?php echo $index;?>"
-					style="z-index:<?php echo $index;?>;
-							left:<?php echo $l; ?>;
-							top:<?php echo $t ?>;">
-					<?php
-					$card_prop = array(
-									'src' => 'assets/img/cards/spade_1.png',
-									'id' => 'img-location-'."$index",
-									'width' => '90',
-									'height' => 'auto'
-								);
-					echo img($card_prop); ?>
-				</div>
-	      	<?php }?>
+	      		<?php $leftCnt = 0; ?>
+	      		<?php foreach ($cardIds as $key => $pathArray) :?>
+	      			<?php
+	      				$length = count($pathArray);
+	      				$firstLeft = $l - (($length - 1) / 2) + $leftCnt; 
+	      			?>
+	      			<?php foreach ($pathArray as $cardId => $path) :?>
+						<div class="card img" id="location-<?php echo $index;?>"
+							style="z-index:<?php echo $index;?>;
+									left:<?php echo $firstLeft.'%'; ?>;
+									top:<?php echo $t == 'auto' ? $t : $t.'%';  ?>;">
+							<?php
+							$card_prop = array(
+											'src' => $path,
+											'id' => 'img-location-'."$index",
+											'width' => '90',
+											'height' => 'auto'
+										);
+							echo img($card_prop); ?>
+						</div>
+	      			<?php endforeach; ?>
+	      			<?php $leftCnt += 1; ?>
+	      		<?php endforeach; ?>
+	      		<?php $cnt++;?>
+	      	<?php endforeach; ?>
 	    </div>
 	</div>
 	<div class="user-area">
