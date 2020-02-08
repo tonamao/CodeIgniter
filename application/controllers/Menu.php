e91586d4774b371087f88732a2bb49a63e53d3e0
tonamao <mao.shimba.tona@gmail.com>
2020-02-08 19:40:20 +0900

    fix menu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -1,14 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Menu extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url_helper');
 		$this->load->helper('html');
+		$this->load->model('masterDataManager');
 	}
-	public function menu()
+	public function games()
 	{
-		$this->load->view('menu');
+		//TODO: ゲーム情報をDBから取ってくる
+		$data['gameInfo'] = $this->masterDataManager->getGameInfo();
+		$this->load->view('menu', $data);
 	}
 	
 }
--- application/views/menu.php
+++ application/views/menu.php
@@ -5,14 +5,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>大富豪</title>
-	<?php echo link_tag('/assets/css/daifugo.css'); ?>
+	<?php echo link_tag('/assets/css/menu.css'); ?>
 	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
 
 </head>
 <body>
 	<div>メニュー</div>
 	<div>
-		<a href="<?php echo site_url('daifugo/start') ;?>" class="btn-square">大富豪</a>
+		<?php foreach ($gameInfo as $key => $game) :?>
+			<div class="game">
+				<div class="icon">
+					<?php
+					$icon_prop = array(
+								'src' => $game['img_path'],
+								'width' => '100',
+								'height' => 'auto'
+							);
+					echo img($icon_prop);?>	
+				</div>
+				<div class="info">
+					<p class="name">
+						<a href="<?php echo site_url('daifugo/rule') ;?>" class="btn-square"><?php echo $game['display_name']?></a>
+					</p>
+					<p class="current-class"><?php echo $game['description']?></p>
+				</div>
+			</div>
+		<?php endforeach; ?>
 	</div>
 </body>
 </html>
